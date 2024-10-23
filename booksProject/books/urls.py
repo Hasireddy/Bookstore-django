ced9a5ed413daeb3296802ac56c74360d0dd278d
Uma Maheswari <100858519+Hasireddy@users.noreply.github.com>
2024-10-23 15:03:12 +0200

    Books views (#2)
    * home view
    
    * registered urls
    
    * configured static settings

--- booksProject/books/urls.py
+++ booksProject/books/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+from . import views
+
 app_name = "books"
 
 urlpatterns = [
-    
-]
+    path("",views.home_view,name="home"),
+    path("book-detail/<str:id>/",views.book_detail,name="book-detail"),
+    path("add-book/",views.add_book,name="add-book"),
+    path('edit-book/<str:id>/', views.edit_book, name='edit-book'),
+    path('delete-book/<str:id>/', views.delete_book, name='delete-book'),
+    ]
