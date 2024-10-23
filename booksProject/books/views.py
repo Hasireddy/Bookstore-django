d40928f5730a6d365b384c213419ee50048844a0
Hasireddy <malikireddy.uma@gmail.com>
2024-10-23 14:49:14 +0200

    home view

--- booksProject/books/views.py
+++ booksProject/books/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+def home_view(request):
+    return HttpResponse("Home page")
+
+def book_detail(request,id):
+    return HttpResponse("Book detail")
+
+def add_book(request):
+    return HttpResponse("Book added")
+
+def edit_book(request,id):
+    return HttpResponse("Book edited")
+
+def delete_book(request,id):
+    return HttpResponse("Book deleted")
