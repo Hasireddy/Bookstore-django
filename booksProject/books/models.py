9f6e130dddd4c63659ddfdc2f09d9af70d13b57e
Uma Maheswari <100858519+Hasireddy@users.noreply.github.com>
2024-10-23 14:42:28 +0200

    Books models (#1)
    * Books Model created
    
    * Templates folder created

--- booksProject/books/models.py
+++ booksProject/books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    isbn = models.CharField(max_length=100)
+    image = models.ImageField(null=False,blank=False,upload_to='images/')
+    created_at = models.DateTimeField(auto_now=True,blank=True)
+    
+    
+    def __str__(self):
+        return f'{self.title}'
+    
+    class Meta:
+        ordering = ['-created_at']
