ad6266e9349521b78b3b0b26b3655909535bcb1d
Hasireddy <malikireddy.uma@gmail.com>
2024-10-24 10:02:25 +0200

    Image folder settings

--- booksProject/booksProject/settings.py
+++ booksProject/booksProject/settings.py
@@ -116,13 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/')
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = BASE_DIR/'static/'
-
-STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
